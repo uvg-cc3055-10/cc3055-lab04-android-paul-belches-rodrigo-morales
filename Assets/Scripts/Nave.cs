55992a9c0592fd202b08319569a2215a2a6c919d
DESKTOP-Q5M49A3\paulb <paulbelches@gmail.com>
2018-04-27 08:07:44 -0600

    Pantalla de menu
    Pantalla para cambio de escena

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour {
+
+
+	public float speed = 5f;
+	private Rigidbody2D rb;
+	public GameObject cam; 
+	bool dead = false;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movX = Input.acceleration.x;
+		rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
+		cam.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		GameController.instance.gameOver = true;		
+	}
+}
---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Scene : MonoBehaviour {
+
+
+    public void juan()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
